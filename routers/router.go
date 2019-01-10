497a378893a9b77413a9fdb28a341016eb5aba3e
Mac <ego@liangjians-MacBook-Air.local>
2019-01-10 19:49:27 +0800

    auto request

--- routers/router.go
+++ routers/router.go
@@ -8,4 +8,5 @@ import (
 func init() {
     beego.Router("/", &controllers.MainController{})
     beego.Router("/crawl_movie", &controllers.CrawlMovieController{}, "*:CrawlMovie")
+    beego.Router("/concurrency/:count",&controllers.MainController{},"get:Concurrency")
 }
